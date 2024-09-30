4ce74aadca9d1bb4ac5f87d6a9e61fd89d42ce38
Danielli7 <148447477+Danielli7@users.noreply.github.com>
2024-09-30 11:37:21 -0300

    ..

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,11 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+    console.log(paragrafo);    
 }
 visualizarInfomacoesGlobais()
